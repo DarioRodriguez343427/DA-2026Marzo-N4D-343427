87542776551bf75c14b6ae6704a9bbdda1ad0986
DarioRodriguez343427 <RR343427@fi365.ort.edu.uy>
2026-04-29 22:47:09 -0300

    carga inicial

---
+++ N4D-343427/src/main/java/DA_2026Marzo/N4D_343427/N4D343427Application.java
@@ -0,0 +1,13 @@
+package DA_2026Marzo.N4D_343427;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class N4D343427Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(N4D343427Application.class, args);
+	}
+
+}
---
+++ N4D-343427/src/test/java/DA_2026Marzo/N4D_343427/N4D343427ApplicationTests.java
@@ -0,0 +1,13 @@
+package DA_2026Marzo.N4D_343427;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class N4D343427ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
